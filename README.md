bd9f57eb1121c5234e6c86b5fae0c0fa611058c7
Alejandro Amaya <aamaya@onapsis.com>
2022-06-01 17:34:21 -0300

    improvements

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # ahorcado-python
 
-Reads the word from the 'ahorcado.txt' file in the same directory the .py is, then chooses a random word and starts the game
+Reads the word from the 'ahorcado.txt' file in the same directory the .py is, then chooses a random word and starts the game.
+One argument can be used: the max tries. It can be 3, 6 or 11 (default 11).
--- ahorcado.py
+++ ahorcado.py
@@ -1,7 +1,9 @@
+import sys
 import random
 
+
 # This is a tuple with a bunch of triple quotes strings that will display in order
-HANGMAN = (
+HANGMAN11 = (
 """
  ------
 |     |
@@ -146,66 +148,236 @@ HANGMAN = (
 """
 )
 
-# This is because the first pic is already there at the start of the program
-MAX_WRONG = len(HANGMAN) - 1
+HANGMAN6 = (
+"""
+ ------
+|     |
+|
+|
+|
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|
+|
+|
+|
+|
+|
+----------
+"""
+   ,
+"""
+ ------
+|     |
+|     0
+|   /-+-
+|
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-/
+|
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|    |
+|    |
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|    | |
+|    | |
+|
+----------
+"""
+)
 
-try:
-    with open("ahorcado.txt", "r") as palabras:
-        word = random.choice(palabras.readlines()).upper()[:-1]
-except FileNotFoundError as fnfe:
-    print("El archivo 'ahorcado.txt' no se encuentra en el mismo directorio que este script")
-    exit(1)
+HANGMAN3 = (
+"""
+ ------
+|     |
+|
+|
+|
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-/
+|
+|
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|
+|
+|
+----------
+"""
+    ,
+"""
+ ------
+|     |
+|     0
+|   /-+-/
+|     |
+|     |
+|    | |
+|    | |
+|
+----------
+"""
+)
 
-# one dash for each letter in word to be guessed except the last character (newline)
-so_far = "_" * len(word)
 
-wrong = 0  # number of wrong guesses player has already made
-used = []  # letters already guessed
-print("Bienvenido al juego del Ahorcado. Buena suerte!")
+def new_game(tries):
+    if tries == 11:
+        hangman = HANGMAN11
+    if tries == 6:
+        hangman = HANGMAN6
+    if tries == 3:
+        hangman = HANGMAN3
 
-# the program will continue while there are still guesses left and the player didn't guess the word
-while wrong < MAX_WRONG and so_far != word:
-    print(HANGMAN[wrong])
-    print("\nLetras usadas hasta ahora:\n", used)
-    print("\nHasta el momento, la palabra es:\n", so_far)
+    # This is because the first pic is already there at the start of the program
+    #MAX_WRONG = len(HANGMAN) - 1
+    max_wrong = tries
 
-    guess = input("\n\nIngrese una letra: ")
-    # this will convert the guess to all uppercase becasue that's how it it is in the tuple
-    guess = guess.upper()
+    try:
+        with open("ahorcado.txt", "r") as palabras:
+            word = random.choice(palabras.readlines()).upper()[:-1]
+    except FileNotFoundError as fnfe:
+        print("El archivo 'ahorcado.txt' no se encuentra en el mismo directorio que este script")
+        exit(1)
 
-    # if they guess the same letter twice, this will print
-    while guess in used:
-        print("La letra {} ya fue ingresada anteriormente".format(guess))
-        guess = input("Ingrese una letra: ")
+    # one dash for each letter in word to be guessed except the last character (newline)
+    so_far = "_" * len(word)
+
+    wrong = 0  # number of wrong guesses player has already made
+    used = []  # letters already guessed
+    print("Bienvenido al juego del Ahorcado. Buena suerte!")
+
+    # the program will continue while there are still guesses left and the player didn't guess the word
+    while wrong < max_wrong and so_far != word:
+        print(hangman[wrong])
+        print("\nLetras usadas hasta ahora:\n", used)
+        print("\nHasta el momento, la palabra es:\n", so_far)
+
+        guess = input("\n\nIngrese una letra: ")
+        # this will convert the guess to all uppercase becasue that's how it it is in the tuple
         guess = guess.upper()
-    # whatever's guessed, gets added to the used list 
-    used.append(guess)
-    # if the guess is correct, it will print the following
-    if guess in word:
-        print("\nSi! La letra ", guess, " se encuentra en la palabra!")
-        # create new so_far to include guess
-        # this means it will replace the blank with the letter
-        new = ""
-        for i in range(len(word)):
-            if guess == word[i]:
-                new += guess
-            else:
-                new += so_far[i]
-        # this redefines so_far to include the letter guessed correctly
-        so_far = new
-    # if the guess is wrong, then it will print the following
+
+        # if they guess the same letter twice, this will print
+        while guess in used:
+            print("La letra {} ya fue ingresada anteriormente".format(guess))
+            guess = input("Ingrese una letra: ")
+            guess = guess.upper()
+        # whatever's guessed, gets added to the used list 
+        used.append(guess)
+        # if the guess is correct, it will print the following
+        if guess in word:
+            print("\nSi! La letra ", guess, " se encuentra en la palabra!")
+            # create new so_far to include guess
+            # this means it will replace the blank with the letter
+            new = ""
+            for i in range(len(word)):
+                if guess == word[i]:
+                    new += guess
+                else:
+                    new += so_far[i]
+            # this redefines so_far to include the letter guessed correctly
+            so_far = new
+        # if the guess is wrong, then it will print the following
+        else:
+            print("\nNo, la letra ", guess, "no se encuentra en la palabra.")
+            # this adds a wrong try
+            wrong += 1
+
+    # if the number of tries reaches the limit, meaning the hangman is hanged, it prints the following
+    if wrong == max_wrong:
+        print(hangman[wrong])
+        print("\nAhorcado!")
+    # otherwise, the person got the word right
     else:
-        print("\nNo, la letra ", guess, "no se encuentra en la palabra.")
-        # this adds a wrong try
-        wrong += 1
+        print("\n\nGanaste!")
+    print("\nLa palabra era: ", word)
+    input("\n\nPresione la tecla 'enter' para salir.")
 
-# if the number of tries reaches the limit, meaning the hangman is hanged, it prints the following
-if wrong == MAX_WRONG:
-    print(HANGMAN[wrong])
-    print("\nAhorcado!")
-# otherwise, the person got the word right
-else:
-    print("\n\nGanaste!")
-print("\nLa palabra era: ", word)
-input("\n\nPresione la tecla 'enter' para salir.")
 
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print("Only one argument is valid, the max tries. It can be 3, 6 or 11 (default 11)")
+        exit()
+
+    tries = int(sys.argv[1]) if len(sys.argv) == 2 else 11
+
+    if tries not in (3, 6, 11):
+        print("The max tries can be 3, 6 or 11 (default 11)")
+        exit()
+
+    new_game(tries)
